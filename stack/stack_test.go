cd762499058f6bb26257a40eb6470df16384346f
HotPotatoC <juandotulung@gmail.com>
2022-03-20 18:00:23 +0700

    test(stack): added testing for stack

---
+++ stack/stack_test.go
@@ -0,0 +1,55 @@
+package stack_test
+
+import (
+	"testing"
+
+	"github.com/HotPotatoC/sture/stack"
+)
+
+func TestAdd(t *testing.T) {
+	s := stack.NewStack[int]()
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+
+	if s.Peek() != 3 {
+		t.Errorf("Expected 3, got %v", s.Peek())
+	}
+
+	if s.IsEmpty() {
+		t.Errorf("Expected stack to not be empty")
+	}
+}
+
+func TestPop(t *testing.T) {
+	s := stack.NewStack[int]()
+	s.Add(1)
+	s.Add(2)
+	s.Add(3)
+
+	s.Pop()
+
+	if s.Peek() != 2 {
+		t.Errorf("Expected 2, got %v", s.Peek())
+	}
+
+	if s.IsEmpty() {
+		t.Errorf("Expected stack to not be empty")
+	}
+
+	s.Pop()
+
+	if s.Peek() != 1 {
+		t.Errorf("Expected 2, got %v", s.Peek())
+	}
+
+	if s.IsEmpty() {
+		t.Errorf("Expected stack to not be empty")
+	}
+
+	s.Pop()
+
+	if !s.IsEmpty() {
+		t.Errorf("Expected stack to be empty")
+	}
+}
